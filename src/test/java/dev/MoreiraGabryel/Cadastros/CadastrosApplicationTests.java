e192c67a2c8531d8e50d6a436a0c4b69fc88cab7
MoreiraGabryel <marcosgabryel25@gmail.com>
2026-01-26 17:17:18 -0300

    Primeira Versão da API De Cadastro

---
+++ src/test/java/dev/MoreiraGabryel/Cadastros/CadastrosApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.MoreiraGabryel.Cadastros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
